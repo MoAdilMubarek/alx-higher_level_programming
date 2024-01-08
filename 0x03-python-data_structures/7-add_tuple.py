01b46840cfa56c6d0913ea0cbeaf5084febda7b2
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-08 11:08:55 -0800

    py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     c = []
     i = 0
     while i < 2:
-        aa = a[i] if len(a) > i else 0
-        bb = b[i] if len(b) > i else 0
+        aa = tuple_a[i] if len(tuple_a) > i else 0
+        bb = tuple_b[i] if len(tuple_b) > i else 0
         c.append(aa + bb)
         i += 1
     return tuple(c)
