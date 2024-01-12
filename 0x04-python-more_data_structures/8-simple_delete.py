3cddf19a1b724e01aee8da2992a70a03d471f060
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-12 10:21:56 -0800

    py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in _dictionary.keys():
-        del a_dictionary['key']
+    if key in _dictionary:
+        del a_dictionary[key]
     return a_dictionary
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    Adictionary = a_dictionary
+    for value in Adictionary.values():
+        value *= 2
+    return Adictionary
