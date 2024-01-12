ca119ed12a1729e3948ef4df2b0a1797b99bb54b
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-12 11:24:11 -0800

    py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None;
+        return None
     mxv = 0
     mxk = None
     for key, val in a_dictionary.items():
