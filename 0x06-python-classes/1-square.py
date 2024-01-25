2a54bbd50d8d8931e85a17244df77ea2cafbd912
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-25 11:52:48 -0800

    py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square moduule."""
+
+class Square:
+    """Define a Square class."""
+
+    def __init__(self, size):
+        """constructor.
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
