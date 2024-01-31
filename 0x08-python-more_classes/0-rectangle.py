3dc8b107dce44e76f114decd0b06bb63029849bd
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-31 10:13:14 -0800

    py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for rec class."""
+
+
+def class Rectangle:
+    """Rectangle class"""
+    pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python3
-"""module for rec class."""
+"""module for rectangle class"""
 
 
-def class Rectangle:
-    """Rectangle class"""
-    pass
+class Rectangle:
+    """defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Method to Instantiation width & height"""
+        self.__width = width
+        self.__height = height
+
+        @property
+        def width(self):
+            """property retrieve width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """set width value"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """property retreive height"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """set height value"""
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
