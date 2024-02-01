2240ef5f1f3dfa9f8b05c2dc9dfc16f873aa71d9
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-31 22:58:30 -0800

    py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -38,12 +38,18 @@ class Rectangle:
                 raise ValueError("height must be >= 0")
             self.__height = value
 
-        def area(self):
-            """return rectangle area"""
-            return self.__width * self.__height
+    def area(self):
+        """return rectangle area"""
+        return self.width * self.height
 
-        def perimeter(self):
-            """return rectangle perimeter"""
-            if self.__width == 0 or self.__height == 0:
-                return 0
-            return 2 * (self.__width + self.__height)
+    def perimeter(self):
+        """return rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+        
+    def __str__(self):
+        rec = ""
+        for i in range(self.height):
+            rec += '#' * self.width + '\n'
+        return rec.rstrip('\n')
