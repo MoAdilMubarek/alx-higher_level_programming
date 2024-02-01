fd7d29f4920d167332f97f6aee6a0695aca463f3
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-02-01 01:07:34 -0800

    py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """Method to Instantiation width & height"""
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
         @property
         def width(self):
@@ -62,4 +62,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
